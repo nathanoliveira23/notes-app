6535179f575389bcbdef8ca4daa310c9ffd6b67c
nathanoliveira23 <nathanoliveira399@gmail.com>
2022-10-22 11:58:02 -0300

    feat: new password validation

--- DTOs/UserDTOs/UserUpdateDTO.cs
+++ DTOs/UserDTOs/UserUpdateDTO.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Notes.DTOs.UserDTOs
 {
     public class UserUpdateDTO : UserDTO
     {
+        [Required(ErrorMessage = "É necessário informar a senha de usuário.")]
+        [MinLength(6, ErrorMessage = "A nova senha deve ter no mínimo 6 caracteres")]
         public string NewPassword { get; set; }
     }
 }
