e407a06c6b2d21fd4f3bff52233df6cebaa386f6
nathanoliveira23 <nathanoliveira399@gmail.com>
2022-10-27 15:41:32 -0300

    feat: user avatar upload

---
+++ Controllers/UserAvatarController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using Notes.Models;
+using Notes.Repository;
+
+namespace Notes.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserAvatarController : BaseController
+    {
+        public UserAvatarController(IUserRepository userRepository) : base(userRepository) { }
+
+        [HttpPatch("upload")]
+        public async Task<IActionResult> AvatarUpload(IFormFile avatar)
+        {
+            // Upload file
+            string currentPath = Directory.GetCurrentDirectory();
+
+            if (!Directory.Exists(Path.Combine(currentPath, "Temp")))
+                Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "Temp"));
+
+            Avatar userAvatar = new()
+            {
+                Title = $"{Guid.NewGuid()} - {avatar.FileName}",
+                Url = Path.Combine(currentPath, "Temp", avatar.FileName),
+                ContentType = avatar.ContentType
+            };
+
+            string imagePath = Path.Combine(Directory.GetCurrentDirectory(), "Temp", userAvatar.Title);
+
+            using (var fs = new FileStream(imagePath, FileMode.Create))
+            {
+                await avatar.CopyToAsync(fs);
+            }
+
+
+            // Save the image name in the database
+            User dbUser = ReadToken();
+
+            dbUser.Name = dbUser.Name;
+            dbUser.Email = dbUser.Email;
+            dbUser.Avatar = userAvatar.Title;
+            dbUser.Password = dbUser.Password;
+
+            _userRepository.Update(dbUser);
+
+            return NoContent();
+        }
+    }
+}
---
+++ Models/Avatar.cs
@@ -0,0 +1,9 @@
+namespace Notes.Models
+{
+    public class Avatar
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string ContentType { get; set; }
+    }
+}
